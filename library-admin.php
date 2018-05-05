bff34bf894d452ca5585de182f40fb8ef67a5655
Matthew Duffy <mattduffy@gmail.com>
2018-05-05 13:49:15 -0700

    Add plugin library stub files.

---
+++ library-admin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * library-admin.php
+ *
+ */
+Class Heatmap_Admin {
+
+  public function init(){
+  
+    return void;
+  }
+}
---
+++ library-site.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * library-site.php
+ *
+ */
+Class Heatmap_Site {
+
+  public function init(){
+  
+    return void;
+  }
+}
